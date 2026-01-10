e2fc7ccb7a8703acc0f8f73e75d6d14131106470
ecommerce <aditya705014@gmail.com>
2026-01-10 14:48:49 +0530

    making frontend

---
+++ frontend/components/ProductCard.jsx
@@ -0,0 +1,18 @@
+function ProductCard({ product }) {
+    const BASEURL = import.meta.env.VITE_DJANGO_BASE_URL;
+    return (
+        <div className="bg-white rounded-xl shadow-md hover:shadow-lg hover:scale-[1.02] transition-transform p-4">
+            <img
+                className="w-full h-56 object-cover rounded-lg mb-4"
+                src={`${BASEURL}${product.image}`}
+                alt={product.name} />
+            <h2 className="text-lg font-semibold text-gray-800">
+                {product.name}
+            </h2>
+            <p className="text-gray-600 font-medium">
+                {product.price}
+            </p>
+        </div>
+    )
+}
+export default ProductCard;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,9 @@
-import { useEffect, useState } from "react";
-function App() {
-  const [products, setProducts] = useState([]);
-  useEffect(() => {
-    fetch('http://127.0.0.1:8000/api/products/')
-      .then(response => response.json())
-      .then(data => setProducts(data))
-      .catch(error => console.error(error));
-  }, []);
-  return (
-    <div className="min-h-screen bg-grey-100 test-gray-800">
-      <h1 className="text-3xl font-bold ">Product List</h1>
-      <div className="container mx-auto p-4">
-        {products.map(products => (
-          <div key={products.id} className="bg-white p-4 rounded shadow mb-4">
-            <h2 className="text-xl font-semibold">{products.name}</h2>
-            <p className="text-gray-600">{products.description}</p>
-            <p className="text-gray-800 font-bold">{products.price}</p>
-          </div>
-        ))}
-      </div>
-
+import ProductList from "../pages/ProductList";
+function App(){
+  return(
+    <div>
+      <ProductList></ProductList>
     </div>
-
-  )
+  );
 }
-
 export default App;
